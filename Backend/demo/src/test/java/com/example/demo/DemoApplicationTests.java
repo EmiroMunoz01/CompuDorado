ccf251a879d5a880a45c11367cbf8827883697d1
Jake <emiro_2001@icloud.com>
2025-08-11 12:08:50 -0500

    Configuracion inicial de nuestra api

--- Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.api;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
