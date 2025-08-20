fcb44df33bb24000e4bba66b6c9c2a00d5301f69
EmiroMunoz01 <emiro_2001@icloud.com>
2025-08-20 15:46:48 -0500

    Hemos creado el IUsuario y el Servicio en donde implementaremos la logica que necesitaremos

--- Backend/demo/src/main/java/com/example/demo/servicio/usuario/IUsuario.java
+++ Backend/demo/src/main/java/com/example/demo/servicio/usuario/IUsuario.java
@@ -1,6 +1,23 @@
 package com.example.demo.servicio.usuario;
 
+import java.util.List;
+
+import com.example.demo.dto.usuario.CrearUsuarioDTO;
+import com.example.demo.dto.usuario.EditarUsuarioDTO;
+import com.example.demo.dto.usuario.UsuarioDTO;
+
 public interface IUsuario {
 
     
+    public List<UsuarioDTO> listarUsuarios();
+
+    public UsuarioDTO listarUsuarioPorId(Integer id);
+
+    public EditarUsuarioDTO editarUsuario(EditarUsuarioDTO editarUsuarioDTO, Integer idUsuario);
+
+    public void eliminarUsuario(Integer id);
+
+    public UsuarioDTO crearUsuarioDTO(CrearUsuarioDTO crearUsuarioDTO);
+
+
 }
--- Backend/demo/src/main/java/com/example/demo/servicio/usuario/UsuarioServicio.java
+++ Backend/demo/src/main/java/com/example/demo/servicio/usuario/UsuarioServicio.java
@@ -1,5 +1,45 @@
 package com.example.demo.servicio.usuario;
 
-public class UsuarioServicio {
+import java.util.List;
+
+import com.example.demo.dto.usuario.CrearUsuarioDTO;
+import com.example.demo.dto.usuario.EditarUsuarioDTO;
+import com.example.demo.dto.usuario.UsuarioDTO;
+
+public class UsuarioServicio implements IUsuario{
+
+
+
+    
+    @Override
+    public List<UsuarioDTO> listarUsuarios() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarUsuarios'");
+    }
+
+    @Override
+    public UsuarioDTO listarUsuarioPorId(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarUsuarioPorId'");
+    }
+
+    @Override
+    public EditarUsuarioDTO editarUsuario(EditarUsuarioDTO editarUsuarioDTO, Integer idUsuario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'editarUsuario'");
+    }
+
+    @Override
+    public void eliminarUsuario(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarUsuario'");
+    }
+
+    @Override
+    public UsuarioDTO crearUsuarioDTO(CrearUsuarioDTO crearUsuarioDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearUsuarioDTO'");
+    }
+
 
 }
